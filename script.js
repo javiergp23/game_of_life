1999feec9c5a1392dea000d9de5011b4740846eb
Javier Gonzalez <javgon2306@gmail.com>
2024-07-19 14:54:11 -0300

    creating let

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-const saludo = "Hola mundo";
+let canvas;
+let ctx;
+let fps = 30;
+
+let canvasX = 500;
+let canvasY = 500;
+
+let tileX, tileY;
+
+//variables de tablero
+let tablero;
+let filas = 100;
+let columnas = 100;
+
+let blanco = '#ffffff';
+let negro = '#000000<s';
+
+function inicializar() {
+    console.log("Inicializando...");
+}
