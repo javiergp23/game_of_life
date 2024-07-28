64d4c1fcb3ee9f1a7adbc5b36f4c3cc6c00cc7ca
Javier Gonzalez <javgon2306@gmail.com>
2024-07-28 13:18:12 -0300

    changes

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 const cambios = document.getElementById('cambios');
+const cambios2 = document.getElementById('cambios');
+const cambios3 = document.getElementById('cambios');
+const cambios4 = document.getElementById('cambios');
+const cambios5 = document.getElementById('cambios');
