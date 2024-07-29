b1d7783ffbde89e247e562a375fa2a7742cd70ef
Javier Gonzalez <javgon2306@gmail.com>
2024-07-29 12:14:23 -0300

    chanches_01

--- main.js
+++ main.js
@@ -1,5 +1,1 @@
 const cambios = document.getElementById('cambios');
-const cambios2 = document.getElementById('cambios');
-const cambios3 = document.getElementById('cambios');
-const cambios4 = document.getElementById('cambios');
-const cambios5 = document.getElementById('cambios');
